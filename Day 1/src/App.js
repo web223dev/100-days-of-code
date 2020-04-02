34afa37afa33578200157443f63b8f5dcd949382
web223dev <ruslanus866@gmail.com>
2020-04-02 22:48:04 +0800

    Finished Randome Quote-Day 1

--- Day 1/src/App.js
+++ Day 1/src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
+import { faTwitterSquare, faTumblrSquare } from '@fortawesome/free-brands-svg-icons'
+import quotes from './quote';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quoteData: quotes,
+      index: 0,
+      quote: 'Happiness is not something readymade. It comes from your own actions.',
+      author: 'Dalai Lama',
+      bgColor: 'rgb(115, 168, 87)'
+    }
+  }
+
+  ChangeEvent() {
+    const { index, quoteData } = this.state;
+    var x = Math.floor(Math.random() * 55) + 100;
+    var y = Math.floor(Math.random() * 55) + 100;
+    var z = Math.floor(Math.random() * 55) + 100;
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+    if (index > 14 || index < 0) {
+      this.setState({ index: 0 })
+    } else {
+      this.setState({
+        index: index + 1,
+        quote: quoteData[index]['quote'],
+        author: quoteData[index]['author'],
+        bgColor: bgColor
+      })
+    }    
+  }
+
+  render() {
+    const { quote, author, bgColor } = this.state;
+    return (
+      <div className="container" style={{backgroundColor: bgColor}}>
+        <div id="wrapper">
+          <QuoteContent
+            handleChangeEvent={() => this.ChangeEvent()}
+            quote={quote}
+            author={author}
+            bgColor={bgColor}
+          />
+          <Switch>
+            <Route path="/"></Route>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
+
+const QuoteContent = ({ handleChangeEvent, quote, author, bgColor }) => {  
+  return (
+    <React.Fragment>
+      <div id="quote-box">
+        <div className="quote-text">
+          <FontAwesomeIcon icon={faQuoteLeft} color={bgColor} />
+          <span id="text" style={{color: bgColor}}> {quote}</span>
+        </div>
+        <div className="quote-author">
+          <span id="author" style={{color: bgColor}}>- {author}</span>
+        </div>
+        <div className="buttons">
+          <Link to="/" id="tweet-quote">
+            <FontAwesomeIcon icon={faTwitterSquare} color={bgColor} />
+          </Link>
+          <Link to="/" id="tumblr-quote">
+            <FontAwesomeIcon icon={faTumblrSquare} color={bgColor} />
+          </Link>
+          <button 
+            id="new-quote"
+            style={{backgroundColor: bgColor}}
+            onClick={() => handleChangeEvent()}>
+              New quote
+          </button>
+        </div>
+      </div>
+      <div id="footer">made by web223dev</div>
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ Day 1/src/quote.js
@@ -0,0 +1,51 @@
+const quotes = [{
+    quote: "You can’t use up creativity. The more you use, the more you have.",
+    author: "Maya Angelou"
+},{
+    quote: "You take your life in your own hands, and what happens? A terrible thing, no one to blame.",
+    author: "Erica Jong"
+},{
+    quote: "It is never too late to be what you might have been.",
+    author: "George Eliot"
+},{
+    quote: "If you want your children to turn out well, spend twice as much time with them, and half as much money.",
+    author: "Abigail Van Buren"
+},{
+    quote: "Everything you’ve ever wanted is on the other side of fear.",
+    author: "George Addair"
+},{
+    quote: "Winning isn’t everything, but wanting to win is.",
+    author: "Vince Lombardi"
+},{
+    quote: "There is only one way to avoid criticism: do nothing, say nothing, and be nothing.",
+    author: "Aristotle"
+},{
+    quote: "Life is what we make it, always has been, always will be.",
+    author: "Grandma Moses"
+},{
+    quote: "I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.",
+    author: "Michael Jordan"
+},{
+    quote: "Eighty percent of success is showing up.",
+    author: "Woody Allen"
+},{
+    quote: "Your time is limited, so don’t waste it living someone else’s life.",
+    author: "Steve Jobs"
+},{
+    quote: "I didn’t fail the test. I just found 100 ways to do it wrong.",
+    author: "Benjamin Franklin"
+},{
+    quote: "You can never cross the ocean until you have the courage to lose sight of the shore.",
+    author: "Christopher Columbus"
+},{
+    quote: "When I let go of what I am, I become what I might be.",
+    author: "Lao Tzu"
+},{
+    quote: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+    author: "Robert Frost"
+},{
+    quote: "Happiness is not something readymade. It comes from your own actions.",
+    author: "Dalai Lama"
+}];
+
+export default quotes;
